9ab3accf5a7dd94facc6a0a57a9007f27a9148ec
Rob Rose <bertra97@gmail.com>
2017-02-04 17:04:34 -0500

    Completed Project 0

---
+++ Projects II/Project 0/joke.cpp
@@ -0,0 +1,24 @@
+/**
+ * File:    joke.cpp
+ * Author:  Robert Rose
+ * Section: 4
+ * Created: 2/4/17
+ * E-mail:  robrose2@umbc.edu
+ * 
+ * Description:
+ * The file with the joke methods in it.
+ */
+
+#include "joke.h"
+
+string opening() {
+    return "Knock, knock";
+}
+
+string setup() {
+    return "Europe";
+}
+
+string punchline() {
+    return "No you're a poo!";
+}
